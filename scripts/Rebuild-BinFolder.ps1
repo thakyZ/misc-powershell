b36e015b79a7771f4ee6e24e5de6f2fe937a22da
Neko Boi Nick <thakyz@outlook.com>
2021-12-22 10:01:17 -0800

    Update Rebuild-BinFolder.ps1

--- scripts/Rebuild-BinFolder.ps1
+++ scripts/Rebuild-BinFolder.ps1
@@ -1,59 +1,113 @@
-$root = "D:\Files\System\Programs\"
-$programs = @("scrcpy"
-              "adb"
-              "Pixivutil"
-              "Git\bin"
-              "Advanced Renamer"
-              "aircrack-ng\bin\64-bit"
-              "bfg"
-              "binvox"
-              "Bulk Rename Utility\All"
-              "CMake\bin"
-              "composer"
-              "cygwin64"
-              "D"
-              "D1"
-              "DMC"
-              "elm-format"
-              "emacs\bin"
-              "fciv"
-              "ffmpeg\bin"
-              "gifski\win"
-              "msys64"
-              "Pear"
-              "php"
-              "Go\bin"
-              "nodejs"
-              "php"
-              "phpcbf"
-              "phpcs"
-              "php-cs-fixer"
-              "Python26"
-              "Python27"
-              "Python37"
-              "r\R-3.5.1patched\bin"
-              "ruby25\Ruby25-x64\bin"
-              "shellcheck"
-              "tidy\bin"
-              "uncrustify",
-              "ipref",
-              "xmllint\bin",
-              "gsfw"
+param(
+	[switch]$output
+)
+
+$root = "%APROG_DIR%\"
+$programs = @("C:\Perl64\bin"
+	            "adb"
+							"bfg"
+							"bin"
+							"Caffeine"
+							"CMake\bin"
+							"colortool"
+							"composer"
+							"cygwin64"
+							"D\dmd2\windows\bin"
+							"D1\dmd\windows\bin"
+							"depot_tools"
+							"depotdownloader"
+							"DMC\dm\bin"
+							"dnSpy"
+							"elm-format"
+							"emacs\bin"
+							"ExportTool"
+							"fciv"
+							"ffmpeg\bin"
+							"FileZilla FTP Client"
+							"gifski\win"
+							"Git\bin"
+							"Git\cmd"
+							"gnuwin32\bin"
+							"Go\bin"
+							"gsfw"
+							"haste"
+							"iperf"
+							"JProfiler\bin"
+							"KiTTY"
+							"nmap"
+							"Pear"
+							"Phing"
+							"php-cs-fixer"
+							"php"
+							"phpcbf"
+							"phpcs"
+							"pngcrush"
+							"PuTTY"
+							"r\R-3.5.1patched\bin"
+							"resize"
+							"ruby25\Ruby25-x64\bin"
+							"shellcheck"
+							"terraform"
+							"tidy\bin"
+							"TortoiseSVN\bin"
+							"uncrusify"
+							"ungit"
+							"VisualD"
+							"whois"
+							"xmllint\bin"
+							"C:\Qt\5.15.2\msvc2019_64\bin"
+							"D:\Progra~2\Gpg4win\bin"
+							"C:\Progra~1\NVIDIA GPU Computing Toolkit\CUDA\v11.5\libnvvp"
+							"C:\Progra~1\NVIDIA GPU Computing Toolkit\CUDA\v11.5\bin"
+							"D:\Progra~1\Guiffy"
+							"C:\Windows\system32\config\systemprofile\AppData\Local\Microsoft\WindowsApps"
+							"C:\Progra~1\NVIDIA Corporation\NVIDIA NvDLISR"
+							"C:\Progra~1\NVIDIA Corporation\Nsight Compute 2021.3.0\"
+							"C:\Progra~1\MariaDB\MariaDB Connector C 64-bit\lib\"
+							"C:\Progra~1\MariaDB\MariaDB Connector C 64-bit\lib\plugin\"
+							"C:\ProgramData\ComposerSetup\bin"
+							"D:\Progra~1\Gpg4win\..\GnuPG\bin"
+							"D:\Progra~2\Gpg4win\bin_64"
+							"D:\Progra~2\Gpg4win\bin"
+							"D:\Progra~2\GnuPG\bin"
+							"D:\ProgramData\Anaconda3"
+							"D:\ProgramData\Anaconda3\Library\mingw-w64\bin"
+							"D:\ProgramData\Anaconda3\Library\usr\bin"
+							"D:\ProgramData\Anaconda3\Library\bin"
+							"D:\ProgramData\Anaconda3\Scripts"
             )
 
-$tempPath = $env:Path;
+$tempPath = "";
+
+$machinePath = ([System.Environment]::GetEnvironmentVariable('path',[System.EnvironmentVariableTarget]::Machine));
+
+$localPath = ([System.Environment]::GetEnvironmentVariable('path',[System.EnvironmentVariableTarget]::User));
 
 ForEach ($program in $programs) {
-  $programPath = "$root$program" ;
-  $tempPath = [regex]::Replace($tempPath, ";"+[regex]::Escape($programPath)+";", ";");
-  if ($program -match $programs[$programs.Length - 1])
-  {
-    $tempPath = [regex]::Replace($tempPath, ";"+[regex]::Escape($programPath), "");
-  }
-  "Adding Path: [$programPath]";
-  $tempPath += ";$programPath";
+	$programPath = "";
+	if ("$($program)".Contains("C:\") -or "$($program)".Contains("D:\") -or "$($program)".Contains("E:\")) {
+		$programPath = "$($program)";
+	} else {
+		$programPath = "$($root)$($program)";
+	}
+	if ($tempPath -eq "") {
+		$tempPath = "$($programPath)";
+	} else {
+		$tempPath = "$($tempPath);$($programPath)";
+	}
+	if ($output) {
+    "Adding Path: [$programPath]";
+	}
 }
 
-[System.Environment]::SetEnvironmentVariable('SystemFiles',$tempPath,[System.EnvironmentVariableTarget]::Machine)
+$newMachinePath = "$machinePath%APROG_FOLD%;"
 
-"Path: [$env:Path]"
+[System.Environment]::SetEnvironmentVariable('APROG_FOLD',$tempPath,[System.EnvironmentVariableTarget]::Machine)
+
+[System.Environment]::SetEnvironmentVariable('path',$newMachinePath,[System.EnvironmentVariableTarget]::Machine)
+
+[System.Environment]::SetEnvironmentVariable('path',$localPath,[System.EnvironmentVariableTarget]::User)
+
+if ($output) {
+	Write-Host "Path added: [$($tempPath)]"
+}
