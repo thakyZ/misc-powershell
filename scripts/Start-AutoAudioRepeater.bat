3c92feb1c86627decc362af2d2042ed32d4eb7bb
Neko Boi Nick <950594+thakyZ@users.noreply.github.com>
2021-12-22 10:11:02 -0800

    Create Start-AutoAudioRepeater.bat

---
+++ scripts/Start-AutoAudioRepeater.bat
@@ -0,0 +1,2 @@
+@echo off
+start /min "" "C:\Progra~1\Virtual Audio Cable\audiorepeater.exe" /SamplingRate:48000 /BitsPerSample:16 /Input:"Line 1 (Virtual Audio Cable)" /Output:"Speakers (THX Spatial - Synapse" /ChanCfg:"5.1 Surround" /AutoStart
