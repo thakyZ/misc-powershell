e1beb471627c663bcd7bcba42ac37dfb8f8f0bb8
Neko Boi Nick <950594+thakyZ@users.noreply.github.com>
2021-12-22 09:47:32 -0800

    Update Set-HypeVStaticIp.ps1

--- scripts/Set-HypeVStaticIp.ps1
+++ scripts/Set-HypeVStaticIp.ps1
@@ -1,4 +1,4 @@
 $Default = "127.0.0.1"
 $WSL = "127.0.0.1"
-Start-Process -Path "C:\Windows\System32\netsh.exe" -ArgumentList "interface ip set address name=`"vEthernet (Default Switch)`" source=static addr=$($Default),255.255.240.0 none"
-Start-Process -Path "C:\Windows\System32\netsh.exe" -ArgumentList "interface ip set address name=`"vEthernet (WSL)`" source=static addr=$($WSL),255.255.240.0 none"
+Start-Process -Path "C:\Windows\System32\netsh.exe" -ArgumentList "interface ip set address name=`"vEthernet (Default Switch)`" source=static addr=$($Default) 255.255.240.0 none"
+Start-Process -Path "C:\Windows\System32\netsh.exe" -ArgumentList "interface ip set address name=`"vEthernet (WSL)`" source=static addr=$($WSL) 255.255.240.0 none"
